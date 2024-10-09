0705d0beddf7b3daa2ca380974ea75a5caeee7a3
ReaRendil <151796249+ReaRendil@users.noreply.github.com>
2024-10-09 18:11:36 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.c
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.c
@@ -5,7 +5,6 @@
 
 // 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
 int solution(const char* my_string, const char* is_suffix) {
-    int answer = 0;
     if(strcmp(my_string+(strlen(my_string)-strlen(is_suffix)),is_suffix)==0) return 1 ;
     else return 0;
 }
