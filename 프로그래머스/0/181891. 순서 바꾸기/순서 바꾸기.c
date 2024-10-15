9bcf2635be63c8b99ec466e7c93fa573d8137838
ReaRendil <151796249+ReaRendil@users.noreply.github.com>
2024-10-15 18:06:19 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.02 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+// num_list_len은 배열 num_list의 길이입니다.
+int* solution(int num_list[], size_t num_list_len, int n) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    int* answer = (int*)malloc(1000);
+    for(int i = 0;i<num_list_len;i++){
+        if(n==num_list_len) n=0;
+        answer[i] =  num_list[n];
+        n++;
+    }
+    return answer;
+}
