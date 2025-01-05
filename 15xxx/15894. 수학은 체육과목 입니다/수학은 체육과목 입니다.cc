ecb1a78130878a4cba9a7698d260b552faf4c678
ReaRendil <151796249+ReaRendil@users.noreply.github.com>
2025-01-05 10:40:14 +0900

    Bronze III

---
+++ 15xxx/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long n; // 정수 변수 n 선언
+    cin >> n; // 사용자로부터 n 값을 입력받음
+    n *= 4; 
+
+    cout << n; // 결과 출력
+    return 0; 
+}
