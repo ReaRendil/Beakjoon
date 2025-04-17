0ef57948b2bcaf26477aaf4d65a9c23c501d38b3
ReaRendil <151796249+ReaRendil@users.noreply.github.com>
2025-04-17 13:42:45 +0900

    Bronze II

---
+++ 33xxx/33702. 비밀번호/비밀번호.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+void mainCode() {
+    int N; cin >> N;
+    if(N%2==0) cout << "0";
+    else  cout  << "8";
+}
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    mainCode();
+}
+
